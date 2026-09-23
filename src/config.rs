8e1fdfab87b8f0a45e105dd55727114f9d004506
XuYing1128 <xuying1128@users.noreply.github.com>
2026-09-23 20:38:08 +0800

    custom rendezvous server: 1.14.120.24

--- src/config.rs
+++ src/config.rs
@@ -114,8 +114,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["1.14.120.24:21116"];
+pub const RS_PUB_KEY: &str = "8wSxwx32DNxx1LOYkxwINmk61GeR8FxyoMpNsJl6R4A=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
